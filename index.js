686e7b81239e9958601e24eaeb09afd5917852fb
boisterous-character-0677 <ramigreenspan@gmail.com>
2019-02-28 16:12:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function deleteFromObjectByKey(object, key){
   return Object.assign({})
 }
 function destructivelyDeleteFromObjectByKey(object, key){
- var newObject = ()
+ var newObject = ({})
  delete object.key
  return newObject
 }
